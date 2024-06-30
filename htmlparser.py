58b9d7267535e20a269640a2e1ae06b447107a84
Lyman Song <linpoche27@gmail.com>
2024-06-30 21:10:05 +0900

    코드 업데이트
    - namuCrawler 클래스 생성
    - namuLoader 클래스 작업 중

--- htmlparser.py
+++ htmlparser.py
@@ -4,10 +4,12 @@ import re
 
 # 웹 페이지 URL
 # url = 'https://namu.wiki/w/%EC%9A%A9%EC%9D%B8%20%EB%B2%84%EC%8A%A4%20119'
-# url = 'https://namu.wiki/w/ILLIT'
-url = 'https://namu.wiki/w/%EC%96%B4%EB%A6%B0%EC%9D%B4%EB%B3%B4%ED%98%B8%EA%B5%AC%EC%97%AD'
+url = 'https://namu.wiki/w/ILLIT'
+# url = 'https://namu.wiki/w/SUMMER%20MOON'
+# url = 'https://namu.wiki/w/%EA%B4%91%EC%A0%80%EC%9A%B0%20%EC%B0%A8%EC%A7%80'
 
-url = 'https://namu.wiki/w/Pok%C3%A9Rogue?from=%ED%8F%AC%EC%BC%80%EB%A1%9C%EA%B7%B8'
+# url = 'https://namu.wiki/w/%ED%98%B8%EB%B9%97(%EA%B0%80%EC%9A%B4%EB%8D%B0%EB%95%85)'
+
 # html 가져오기
 session = requests.Session()
 html_doc = session.get(url)
@@ -32,61 +34,100 @@ def get_class_name(keyword, soup):
             print('No class attribute')
 
 
-## 목차(TOC) 정보 추출 및 TOC 딕셔너리 구성
-toc = soup.find_all("div", class_ = 'toc-indent')[0]
+"""## 목차(TOC) 정보 추출 및 TOC 딕셔너리 구성"""
+toc = soup.find("div", class_ = 'toc-indent')
 toc_dict = dict()
 
-# 만약 프로필이 있는 문서라면 가장 앞에 구성
+"""만약 프로필이 있는 문서라면 가장 앞에 구성"""
 html_str = str(soup)
 start_pos = html_str.find(str('<body>')) + len(str('<body>'))
-end_pos = html_str.find(str('<div class="wiki-macro-toc"'))
+toc_str = str(soup.find("div", class_='wiki-macro-toc'))
+end_pos = html_str.find(toc_str)
 between_content = html_str[start_pos:end_pos]
 soup_between = BeautifulSoup(between_content, 'html.parser')
 
 if len(profile := soup_between.find_all('div', class_ = 'wiki-table-wrap table-right')) > 0:
-    # print(profile[-1].get_text())
-    toc_dict['s-p'] = ('profile', profile[-1]) # 마지막엔 각주 영역
+    toc_dict['s-p'] = (("0.",'PROFILE'), profile[-1]) # dictionary의 첫번째 아이템으로 넣기
 
-# 딕셔너리 형태로 저장(key = s-#.#.#, value = (목차 명, 목차 element))
+
+"""목차 아이템들을 하나씩 딕셔너리에 저장(key = s-#.#.#, value = (목차 명, 목차 element))"""
+pattern = r'^(\d+\.)+' # 제목 파싱용 패턴
+
 for i, e in enumerate(toc.find_all("span", class_  = "toc-item")):
-    toc_dict[e.find('a')['href'].replace("#", "")] = (e.get_text(), soup.find('a', id=e.find('a')['href'].replace("#", "")))
-toc_dict['s-f'] = ('footnote', soup.find("div", class_ = 'wiki-macro-footnote')) # 마지막엔 각주 영역
+    item_value = e.get_text()
+    numbers = re.match(pattern, item_value).group()
+    text = re.sub(pattern, '', item_value).strip()
+    toc_dict[e.find('a')['href'].replace("#", "")] = ((numbers, text), soup.find('a', id=e.find('a')['href'].replace("#", "")))
+
+"""마지막 원소로 각주영역 저장, 없다면 None 저장"""
+toc_dict['s-f'] = (("EOD.", 'FOOTNOTES'), soup.find("div", class_ = 'wiki-macro-footnote')) # 마지막엔 각주 영역
 
 # 목차 프린트
 print(f"\nDocument of {soup.find("a", href = "/w/"+url.split("/w/")[-1].split("?")[0]).get_text()}\n")
 print("\n================== Table of Contents  ==================\n")
 for k, v in toc_dict.items():
-    num = k.count(".")
-    print("".join(["\t"] * num) + v[0])
+    num = v[0][0].count(".")
+    if num == 0:
+        print(v[0][0] + " " + v[0][1])
+    else:
+        print("".join(["\t"] * (num - 1)) + v[0][0] + " " + v[0][1])
 
+def strip_footnotes(ele):
+    content_list = []
+    prv_tag = None
+    for c in ele.descendants:
+        if c.name == 'a' and 'wiki-fn-content' in c.get('class', []):
+            # 'wiki-fn-content' 클래스를 가진 'a' 태그를 리스트에 추가
+            content_list.append(f"({c['href']}; {c['title']})")
+            prv_tag = True
+        elif isinstance(c, str):
+            # 텍스트 노드를 리스트에 추가
+            if prv_tag == True:
+                prv_tag = False
+                continue
+            content_list.append(c.string.strip())
+            prv_tag = False
+    return "".join(content_list)
 
-def get_content_between_heandings(soup, start_tag, end_tag):
-    # HTML 콘텐츠를 문자열로 변환
+def get_content_between_tags(head, start_tag, end_tag):
+    """두개의 태그 사이의 wiki-paragraph 정보 추출"""
     html_str = str(soup)
 
-    # 시작 태그와 끝 태그의 위치를 찾기
-    start_pos = html_str.find(str(start_tag)) + len(str(start_tag))
+
+    # 시작 태그와 끝 태그의 위치를 찾아 사이의 컨텐츠를 추출, 임시 soup로 만듦
+    start_pos = html_str.find(str(start_tag))
     end_pos = html_str.find(str(end_tag))
-
-    # 시작 태그와 끝 태그 사이의 콘텐츠를 추출
     between_content = html_str[start_pos:end_pos]
-
-    # 추출한 콘텐츠를 다시 파싱
     soup_between = BeautifulSoup(between_content, 'html.parser')
 
-    # 특정 클래스명을 가진 엘리먼트를 수집
+    # wiki-paragraph를 가진 엘리먼트를 수집, 텍스트 컨텐츠 추출
     elements_between = soup_between.find_all('div', class_='wiki-paragraph')
 
-    # 결과 출력
-    for element in elements_between:
-        # print(element)
-        print(element.get_text())
-
-def get_item_and_next(dictionary, target_key):
-    keys = list(dictionary.keys())
-    values = list(dictionary.values())
+    if len(elements_between) == 0:
+        # 설명이 아예 없는 경우 None 반환
+        return (head, None)
+    elif (ext_icon := elements_between[0].find("img", alt = '상세 내용 아이콘')) != None:
+        # 타 문서로 설명이 대체된 경우엔 링크 반환
+        ext_link = elements_between[0].find("a", class_ = "wiki-link-internal")['href']
+        return (head, ext_link)
+    else: 
+        # 설명이 있는 경우엔 get_text()로 텍스트 반환
+        text_content = []
+        for element in elements_between:
+            # 만약 각주가 있는 엘리먼트라면 각주를 strip하는 함수 적용,  아니면 그냥 일반 get_text() 적용
+            if element.find("a", class_ = 'wiki-fn-content') != None:
+                text_content.append(strip_footnotes(element))
+            else:
+                text_content.append(element.get_text())
+            
+        return (head, text_content)
     
-    if target_key in dictionary:
+def get_item_and_next(target_key):
+    """toc_dict에서 현재 헤드의 다음 헤드 값을 반환"""
+    keys = list(toc_dict.keys())
+    values = list(toc_dict.values())
+    
+    if target_key in toc_dict:
         index = keys.index(target_key)
         if index < len(keys) - 1:
             return keys[index + 1], values[index + 1]
@@ -95,9 +136,30 @@ def get_item_and_next(dictionary, target_key):
     else:
         return None
 
-def get_content_heading(soup, toc_dict, heading_idx):
+def get_content_heading(heading_idx):
+    """헤드 값의 컨텐츠 내용을 반환"""
     start_tag = toc_dict.get(heading_idx)[1]
-    end_tag = get_item_and_next(toc_dict, heading_idx)[1][1] 
-    content = get_content_between_heandings(soup, start_tag= start_tag, end_tag= end_tag)
+    try:
+        end_tag = get_item_and_next(heading_idx)[1][1] 
+    except(KeyError): #마지막 헤딩(보통 각주영역)의 경우엔 직접 로딩
+        if toc_dict.get(heading_idx)[1] == None: # 각주가 없는 문서
+            return (toc_dict.get(heading_idx)[0], None)
+        else:
+            return (toc_dict.get(heading_idx)[0], toc_dict.get(heading_idx)[1].get_text())
+    content = get_content_between_tags(head = toc_dict.get(heading_idx)[0], start_tag= start_tag, end_tag= end_tag)
+
     return content
 
+
+
+# html_str = str(soup)
+
+
+# # 시작 태그와 끝 태그의 위치를 찾아 사이의 컨텐츠를 추출, 임시 soup로 만듦
+# start_pos = html_str.find(str(toc_dict.get("s-3.4.1")[1]))
+# end_pos = html_str.find(str(toc_dict.get("s-3.4.2")[1]))
+# between_content = html_str[start_pos:end_pos]
+# soup_between = BeautifulSoup(between_content, 'html.parser')
+# elements_between = soup_between.find_all('div', class_='wiki-paragraph')
+
+get_content_heading("s-3.4.1")
---
+++ namu_loader.py
@@ -0,0 +1,45 @@
+import os
+from namu_crawler import namuCrawler
+base_url = 'https://namu.wiki'
+url = 'https://namu.wiki/w/%ED%98%B8%EB%B9%97(%EA%B0%80%EC%9A%B4%EB%8D%B0%EB%95%85)'
+url = 'https://namu.wiki/w/ILLIT'
+# url = 'https://namu.wiki/w/%ED%94%84%EB%A1%9C%EB%AF%B8%EC%8A%A4%EB%82%98%EC%9D%B8'
+# url = "https://namu.wiki/w/%EA%B4%91%EC%A0%80%EC%9A%B0%20%EC%B0%A8%EC%A7%80"
+
+base_nc = namuCrawler(url = url, hop = 0) ## 타겟 베이스 문서 크롤러 생성
+base_nc.construct_toc()
+base_nc.print_toc()
+
+def get_total_content(parent_item, url, hop, max_hop):
+    sub_nc = namuCrawler(url = url, hop = hop)
+    sub_nc.construct_toc()
+    # print(sub_nc.get_doc_title(), parent_item, sub_nc.hop, max_hop)
+    to_return = ""
+    for k, v in sub_nc.toc_dict.items():
+        cur_toc_item, content = sub_nc.get_content_heading(k)
+        
+        if type(content) == str and f'/w/' in content: # content가 링크 대체이면서 
+            if sub_nc.hop < max_hop: #현재 문서의 hop이 max_hop보다 적거나 같으면 더 들어가기
+                content = get_total_content(parent_item = parent_item, url = base_url + content, hop = sub_nc.hop + 1, max_hop = max_hop)
+            else: # max_hop과 같으면 그냥 링크로 대체한다고만 써주기
+                content = f"{cur_toc_item[1]}: 다음 문서로 대체 설명: {base_url + content}"
+        else: # 일반 설명은 {현재 목차 : 설명} 꼴로 구성
+            content = f"{cur_toc_item[1]}: {" ".join(content) if type(content) == list else ""}"
+        
+        to_return = to_return + "\n" + content + "\n"
+
+    return to_return
+
+def get_a_content(key, max_hop):
+    cur_toc_item, content = base_nc.get_content_heading(key)
+    if content == None:
+        return (cur_toc_item, None)
+    elif type(content) == str and '/w/' in content: # content가 링크 대체라면
+        return (cur_toc_item, get_total_content(parent_item = base_nc.toc_dict.get(key)[0], url = base_url + content, hop = base_nc.hop + 1, max_hop = max_hop))
+    else:
+        return (cur_toc_item, "".join(content))
+    
+
+for k, v in base_nc.toc_dict.items():
+    print(">> ", k, v[0][1])
+    print("\t", get_a_content(k, 2)[1])
